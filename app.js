373d4a3d731b3c357c6d3ab8e0bb65e71d04f85a
Alexandre <alexandrepereiraassano@gmail.com>
2024-05-14 22:28:36 +0900

    completed simple todo react app

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 require("dotenv").config();
-const initdata = require("./data-init.js");
+const { initTodo, postTodo, updateTodo, deleteTodo } = require("./data-init.js");
 
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use("/", express.static(path.join(__dirname, "/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, PATCH, DELETE, OPTIONS');
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html");
 });
 
+
 app.get("/datajson", (req, res) => {
   try {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.status(201).json(initdata);
+    res.status(201).json(initTodo);
+  } catch(err) {
+    console.error(err);
+  }
+});
+
+app.post("/datajson", (req, res) => {
+  const postedTodo = postTodo(req.body);
+  res.json(postedTodo);
+});
+
+app.patch("/datajson/:id", (req, res) => {
+  const updatedTodo = updateTodo(req.body);
+  res.json(updatedTodo);
+});
+
+app.delete("/datajson/:id", (req, res) => {
+  try {   
+    deleteTodo(req.params.id);
+    res.status(201).json({ "message": "ok" });
   } catch(err) {
     console.error(err);
   }
--- data-init.js
+++ data-init.js
@@ -1,4 +1,4 @@
-const initdata = [
+const initTodo = [
   { id: 1, content: "Saorina Mieken", editing: false },
   { id: 2, content: "Suzuka G garden Mieken", editing: false },
   { id: 3, content: "Okudo Sports Center Katsushika-ku Tokyo", editing: false },
@@ -6,4 +6,40 @@ const initdata = [
   { id: 5, content: "Tokyo Gynasium Minato-ku Tokyo", editing: false }
 ];
 
-module.exports = initdata;
+const postTodo = todo => {
+  initTodo.push(todo);
+  return todo;
+};
+
+const updateTodo = todo => {
+  let updatedTodo = {};
+  initTodo.map(_todo => {
+    if(_todo.id === todo.id){
+      _todo.content = todo.content;
+      _todo.editing = todo.editing;
+      updatedTodo = { ..._todo };
+    } else {
+      return _todo;
+    }
+  });
+  return updatedTodo;
+};
+
+const deleteTodo = id => {
+  let deleteIndex;
+  const deleteId = initTodo.map((todo, index) => {
+    if(todo.id === parseInt(id))
+      deleteIndex = index;
+  });
+  initTodo.splice(deleteIndex, 1);
+};
+
+module.exports = { initTodo, postTodo, updateTodo, deleteTodo };
+
+/*
+  { id: 1, content: "Saorina Mieken", editing: false },
+  { id: 2, content: "Suzuka G garden Mieken", editing: false },
+  { id: 3, content: "Okudo Sports Center Katsushika-ku Tokyo", editing: false },
+  { id: 4, content: "Aquatic Center Tatsumi Tokyo", editing: false },
+  { id: 5, content: "Tokyo Gynasium Minato-ku Tokyo", editing: false }
+*/
